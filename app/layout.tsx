0126f7a66d4e8a65729eee7a3882a0f0ea1a1aef
neerajkashetty <neeraj.kasheety29@gmail.com>
2024-09-25 01:25:57 -0400

    init

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import {Inter} from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: "Neeraj Kasheety",
+  description: "Software Engineer with 5 years of experience",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 h-[5000px]`}
+      >
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] h-[31.25rem] w-[31.25rem] -z-10 blur-[9rem] rounded-full right-[-11rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] h-[31.25rem] w-[50.25rem] -z-10 blur-[9rem] rounded-full left-[-15rem] sm:w-[68.75rem] md:left-[] "> </div>
+        <Header/>
+        {children}
+
+      </body>
+    </html>
+  );
+}
