21bf50430df8d7edf72af99f49f4f6bb243f07f5
neerajkashetty <neeraj.kasheety29@gmail.com>
2024-10-05 21:34:21 -0400

    add skills section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import About from "@/components/about";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDiv from "@/components/sectiondiv";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <SectionDiv />
       <About />
       <Projects />
+      <Skills />
     </div>
   );
 }
