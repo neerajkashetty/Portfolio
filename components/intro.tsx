559c1a95d89a68e21cb97468175c96328d923229
neerajkashetty <neeraj.kasheety29@gmail.com>
2024-10-26 14:06:29 -0400

    chore: clean the unused code

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { delay, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
--- lib/data.ts
+++ lib/data.ts
@@ -1,8 +1,3 @@
-import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
-
 export const links = [
   {
     name: "Home",
